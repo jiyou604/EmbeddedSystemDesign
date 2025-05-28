9f780a85dedd1cbc94f6ae2b7d16987c6a42f780
jiyou604 <hjy207289@hanyang.ac.kr>
2025-05-28 16:47:42 +0900

    onnx update2

--- utils/torch2onnx.py
+++ utils/torch2onnx.py
@@ -1,12 +1,12 @@
 import torch
 
 # PyTorch 모델 로드
-model_path = './custom_model/PB/exp2/weights/best.pt'
+model_path = '../custom_model/PB/exp2/weights/best.pt'
 model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path)
 model.eval()
 
 # ONNX 변환용 더미 입력 (YOLO는 640x640 기본)
-dummy_input = torch.randn(1, 3, 640, 640)
+dummy_input = torch.randn(1, 3, 480, 480)
 
 # ONNX로 내보내기
 torch.onnx.export(
@@ -22,4 +22,4 @@ torch.onnx.export(
     }
 )
 
-print("✅ ONNX 모델 변환 완료: yolov5_custom.onnx")
+print("✅ ONNX 모델 변환 완료: yolov5_custom_480x480.onnx")
