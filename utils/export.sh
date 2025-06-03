6c1419707eeba5f1b3335685fa782f4931d91991
jiyou604 <jiyou@DESKTOP-CHSRLKV>
2025-06-03 22:24:59 +0900

    fp16 updated

--- utils/export.sh
+++ utils/export.sh
@@ -8,7 +8,7 @@ PREP_PATH="./custom_model/augmented/exp/weights/best_simplified_preprocessed.onn
 
 # === Step 1: Export to ONNX ===
 echo "Exporting yolov5 to ONNX..."
-python ./yolov5/export.py --weights "$INPUT_PATH" --img 480 --include torchscript onnx #--img 680
+python ./yolov5/export.py --weights "$INPUT_PATH" --img 320 --include torchscript onnx #--img 680
 
 # === Step 2: Simplify the model ===
 echo "Simplifying the model..."
@@ -18,6 +18,7 @@ python3 -m onnxsim "$ONNX_PATH" "$SIMP_PATH"
 echo "Applying dynamic quantization..."
 echo "Preprocessing..."
 python -m onnxruntime.quantization.preprocess --input "$SIMP_PATH" --output "$PREP_PATH"
-echo "Applying dynamic quantization..."
+echo "Quantizing into FP16..."
 python3 ./utils/quantize.py
 
+echo "done"
