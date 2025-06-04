955ff33a625d31c219754e01f260e2dd1bf4114e
yerim <goldbabyerim@hanyang.ac.kr>
2025-06-04 17:50:51 +0900

    maze modify

--- inference_obs.sh
+++ inference_obs.sh
@@ -1,1 +1,2 @@
-python ./yolov5/detect.py --source ./mazes/test/images/KakaoTalk_20250531_024037092_01.jpg --weights ./custom_model/Obstacles/exp2/weights/best.pt
+python ./yolov5/detect.py --source ./mazes/test/imagesKakaoTalk_20250604_171357572_01.jpg --weights ./custom_model/Obstacles/exp2/weights/best.pt
+#python ./yolov5/detect.py --source "./mazes/test/images/*.jpg" --weights ./custom_model/Obstacles/exp2/weights/best.pt
